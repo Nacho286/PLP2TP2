7ec89c4f8e90977f40697507e1c80b772b2b149a
ignacio roberto rodriguez <ignacio.igrodriguez@mercadolibre.com>
2017-10-05 19:08:24 -0300

    Cambios parece funcionar los primeros.

--- esqueleto.pl
+++ esqueleto.pl
@@ -25,13 +25,13 @@ adyacenteEnRango(T,F1,C1,F2,C2) :- adyacente(F1,C1,F2,C2), enRango(T,F2,C2).
 
 
 %contenido(+?Tablero, ?Fila, ?Columna, ?Contenido)
-contenido(T,F,C,X):-valido(T,F,C),obtener(T,F,C,Y),ground(Y),Y == X.
-contenido(T,F,C,X):-not(ground(F)),ground(C),matriz(T,F1,_),between(1,F1,F),ground(Y),obtener(T,F,C,Y),Y == X.
-contenido(T,F,C,X):-not(ground(C)),ground(F),matriz(T,_,C1),between(1,C1,C),ground(Y),obtener(T,F,C,Y),Y == X.
-contenido(T,F,C,X):-not(ground(F)),not(ground(C)),matriz(T,F1,C1),between(1,F1,F),between(1,C1,C),obtener(T,F,C,Y),ground(Y),Y == X.
+contenido(T,F,C,X):-valido(T,F,C),obtener(T,F,C,X).
+contenido(T,F,C,X):-not(ground(F)),ground(C),matriz(T,F1,_),between(1,F1,F),obtener(T,F,C,X).
+contenido(T,F,C,X):-not(ground(C)),ground(F),matriz(T,_,C1),between(1,C1,C),obtener(T,F,C,X).
+contenido(T,F,C,X):-not(ground(F)),not(ground(C)),matriz(T,F1,C1),between(1,F1,F),between(1,C1,C),obtener(T,F,C,X).
 
 %disponible(+Tablero, ?Fila, ?Columna)
-disponible(T,F,C):-not((not(contenido(T,F,C,_)),adyacenteEnRango(T,F,C,F1,C1),contenido(T,F1,C1,_))).
+disponible(T,F,C):-valido(T,F,C),libre(T,F,C),forall(adyacenteEnRango(T,F,C,F1,C1),libre(T,F1,C1)).
 
 %puedoColocar(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
 puedoColocar(Cant,D,T,F,C):-valido(T,F,C),mover(Cant,D,T,F,C).
@@ -42,7 +42,7 @@ puedoColocar(Cant,D,T,F,C):-not(ground(F)),not(ground(C)),matriz(T,F1,C1),betwee
 %ubicarBarcos(+Barcos, +?Tablero)
 
 %completarConAgua(+?Tablero)
-completarConAgua(T):-matriz(T,F,C),between(1,F,N),between(1,C,M),disponible(T,N,M),colocar(T,N,M,"~").
+completarConAgua(T):-forall(libre(T,X,Y),contenido(T,X,Y,"~")).
 
 %golpear(+Tablero, +NumFila, +NumColumna, -NuevoTab)
 
@@ -54,15 +54,15 @@ completarConAgua(T):-matriz(T,F,C),between(1,F,N),between(1,C,M),disponible(T,N,
 %valido(+?Tablero, ?Fila, ?Columna)
 valido(T,F,C):-ground(F),ground(C),matriz(T,F1,C1),F=<F1,C=<C1.
 
-%colocar(+?Tablero, +Fila, +Columna,+Contenido)
-colocar(T,F,C,X):-valido(T,F,C),nth1(F,T,L),nth1(C,L,Y),X = Y.
+%libre(+?Tablero,?Fila,?Columna)
+libre(T,F,C):-contenido(T,F,C,Y),not(ground(Y)).
 
 %obtener(+Tablero, +Fila, +Columna,-Contenido)
 obtener(T,F,C,X):-nth1(F,T,L),nth1(C,L,X).
 
-%mover(+Cantiad,+Direccion,+Tablero, +F1, +C1,-F2,-C2)
-mover(Cant,vertical,T,F,C):-F1 is F+Cant,forall(between(F,F1,X),disponible(T,X,C)).
-mover(Cant,horizontal,T,F,C):-C1 is C+Cant,forall(between(C,C1,X),disponible(T,F,X)).
+%mover(+Cantiad,+Direccion,+Tablero, +F1, +C1)
+mover(Cant,vertical,T,F,C):-F1 is F+Cant-1,forall(between(F,F1,X),disponible(T,X,C)).
+mover(Cant,horizontal,T,F,C):-C1 is C+Cant-1,forall(between(C,C1,X),disponible(T,F,X)).
 
 %------------------Tests:------------------%
 
